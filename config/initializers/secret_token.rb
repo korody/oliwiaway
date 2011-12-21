7255921d7d160f8312dcd6773c0e66b3332d9cc4
korody <marcoskorody@hotmail.com>
2011-12-21 00:18:19 -0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oliwiaway::Application.config.secret_token = 'aefea36bfb1036088fda3f1d546c04654c5e812e05c6023cef04ad1223f6efc4373a91f160ba4929f8025ad96edc812514da45f6b8063abe4db025de5280f0cc'
