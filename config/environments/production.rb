67ee3556eb95e3577cadcf9ca395328dc2d76a6e
korody <marcoskorody@hotmail.com>
2012-01-08 12:25:12 -0200

    Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,9 @@ Oliwiaway::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
+
+  config.assets.precompile = [/^[a-zA-Z]*\..*/]
 
   # Generate digests for assets URLs
   config.assets.digest = true
