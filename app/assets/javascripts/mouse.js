f18d4d00265139389a954567e3cd0ffa2245f686
korody <marcoskorody@hotmail.com>
2012-01-08 23:51:25 -0200

    heroku2

--- app/assets/javascripts/mouse.js
+++ app/assets/javascripts/mouse.js
@@ -1,9 +1,7 @@
-$(function() {
-
-('.scrollable').bind('mousewheel', function(event,delta){
+$('.scrollable').bind('mousewheel', function(event,delta){
  if (delta > 0) {
-   // mousewheel is going left; 
+   // mousewheel is going up; 
  } else {
-   // mousewheel is going right; 
+   // mousewheel is going down; 
  }
 });
