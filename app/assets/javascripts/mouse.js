95195222cb9aaa2a0eccae92b5b632f899576b19
korody <marcoskorody@hotmail.com>
2012-01-08 23:31:01 -0200

    heroku1

---
+++ app/assets/javascripts/mouse.js
@@ -0,0 +1,9 @@
+$(function() {
+
+('.scrollable').bind('mousewheel', function(event,delta){
+ if (delta > 0) {
+   // mousewheel is going left; 
+ } else {
+   // mousewheel is going right; 
+ }
+});
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Oliwiaway::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   #config.assets.precompile = [/^[a-zA-Z]*\..*/]
 
