5ee24fc5d5a3f1c51030fd2d1ba88202fae8b97e
korody <marcoskorody@hotmail.com>
2012-01-16 18:55:31 -0200

    Oliwia Growing

--- app/assets/javascripts/scroller.js
+++ app/assets/javascripts/scroller.js
@@ -1,4 +1,19 @@
 $(function() {
+				$(".scrollable").scrollable({ mousewheel: true });
 
-$(".scrollable").scrollable().navigator()
-});	
+				$('.home').click(function() {
+					$(".scrollable").scrollable().begin(1500);
+				});
+
+				$('.controls a').click(function() {
+					var currentIndex = ($(".scrollable").scrollable().getIndex());
+					if (currentIndex > 0) {
+						$('.home').css('visibility','visible')
+					} else if (currentIndex === 0) {
+						$('.home').css('visibility','hidden')
+					}
+				})
+
+			});
+
+	
